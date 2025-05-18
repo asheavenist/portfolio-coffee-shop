fbc8be5113fcfb01f0ffe2296a97380e1ec3afe2
asheavenist <asheavenist@yahoo.com>
2025-05-18 12:47:24 +0700

    UI completed. Responsiveness included 💪

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav'),
+  hamburgerMenu = document.querySelector('#hamburger-menu');
+
+hamburgerMenu.onclick = () => {
+  navbarNav.classList.toggle('active');
+}
+
+document.addEventListener('click', function(e) {
+  if(!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+})
