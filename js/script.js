5780e437410591342fbba8da753c8f1e4a2e0c1a
asheavenist <asheavenist@yahoo.com>
2025-05-26 11:17:26 +0700

    Cart UI all set! Added smooth slide animation for better UX

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav'),
   hamburgerMenu = document.querySelector('#hamburger-menu'),
-  searchButton = document.querySelector('#search-button')
+  searchButton = document.querySelector('#search-button'),
+  shoppingCartButton = document.querySelector('#shopping-cart-button')
 
 hamburgerMenu.onclick = () => {
   navbarNav.classList.toggle('active');
@@ -9,22 +10,32 @@ hamburgerMenu.onclick = () => {
 // Toggle class active untuk search form
 const searchForm = document.querySelector('.search-form'),
   searchBox = document.querySelector('#search-box')
-  // searchLabel = document.querySelector('.search-label');
 
 document.querySelector('#search-button').onclick = (e) => {
   searchForm.classList.toggle('active');
   searchBox.focus();
   e.preventDefault();
-  // searchLabel.classList.toggle('active');
+}
+
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector('.shopping-cart')
+
+document.querySelector('#shopping-cart-button').onclick = () => {
+  shoppingCart.classList.toggle('active');
 }
 
 // Klik di luar sidebar untuk menghilangkan nav
 document.addEventListener('click', function(e) {
   if(!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove('active');
+    navbarNav.classList.remove('active')
+    e.preventDefault();
   }
 
   if(!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove('active');
+  }
+
+  if(!shoppingCartButton.contains(e.target) && !shoppingCartButton.contains(e.target)) {
+    shoppingCart.classList.remove('active');
   }
 })
